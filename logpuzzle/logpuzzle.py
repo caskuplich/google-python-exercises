04a5dd6d7666cd26bd45f7e194f669e84239d320
Cassiano Kuplich <crkuplich@openmailbox.org>
2016-08-29 21:17:48 -0300

    Finish log puzzle

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # Copyright 2010 Google Inc.
+# Copyright 2016 Cassiano Rocha Kuplich
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
@@ -9,14 +10,24 @@
 import os
 import re
 import sys
-import urllib
+import urllib.request
 
 """Logpuzzle exercise
 Given an apache logfile, find the puzzle urls and download the images.
 
 Here's what a puzzle url looks like:
-10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
+10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg \
+HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; \
+rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
+
+
+def url_idx(url):
+    match = re.search(r'-\w+-(\w+)\.jpg', url)
+    if match:
+        return match.group(1)
+    else:
+        return url
 
 
 def read_urls(filename):
@@ -24,7 +35,26 @@ def read_urls(filename):
     extracting the hostname from the filename itself.
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
-    # +++your code here+++
+
+    # Extract the hostname.
+    match = re.search(r'_(.+)', filename)
+    if match:
+        hostname = 'http://' + match.group(1)
+    else:
+        sys.stderr.write("log file name doesn't contain hostname\n")
+        sys.exit(1)
+
+    # Extract the puzzle urls from the log file.
+    urls = []
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+    tuples = re.findall(r'GET\s(\S*puzzle\S*)\sHTTP', text)
+    for path in tuples:
+        url = hostname + path
+        if url not in urls:
+            urls.append(url)
+    return sorted(urls, key=url_idx)
 
 
 def download_images(img_urls, dest_dir):
@@ -35,7 +65,21 @@ def download_images(img_urls, dest_dir):
     with an img tag to show each local image file.
     Creates the directory if necessary.
     """
-    # +++your code here+++
+    # Create the given directory if necessary.
+    os.makedirs(dest_dir, exist_ok=True)
+
+    # Create the index.html file.
+    f = open(os.path.join(dest_dir, 'index.html'), 'w')
+    f.write('<html>\n<body>\n')
+    for i in range(len(img_urls)):
+        url = img_urls[i]
+        print('Retrieving ' + url + ' ...')
+        img_filename = 'img%d' % i
+        # Download the image of the current url.
+        urllib.request.urlretrieve(url, os.path.join(dest_dir, img_filename))
+        f.write('<img src="%s">' % img_filename)
+    f.write('\n</body>\n</html>')
+    f.close()
 
 
 def main():
